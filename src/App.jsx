9edae49b6af0b59b3ad63ddef4cf38928e5c21c5
Pedro <pedrolucascabral4@gmail.com>
2026-06-18 22:56:21 -0300

    feat: implement task management features with AddTask and Tasks components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,74 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import {v4} from "uuid";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description:
+        "Estudar programação para se tornar um desenvolvedor fullstack",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Fazer exercícios físicos",
+      description: "Fazer exercícios físicos para manter a saúde em dia",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Ler um livro",
+      description: "Ler um livro para expandir o conhecimento",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted,
+        };
+      }
+
+      return task;
+    });
+    setTasks(newTasks);
+  }
+
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
+
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
   return (
-    <div>
-      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
-      <Tasks/>
-      <AddTask/>
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
+      </div>
     </div>
   );
 }
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,27 @@
-function Tasks() {
-  return <h1>Tasks</h1>
+import { TrashIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
+
+function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
+  return(
+    <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      {tasks.map((task) => (
+        <li key={task.id} className="flex gap-2">
+          <button 
+          onClick={() => onTaskClick(task.id)} 
+          className={`bg-slate-400 text-left text-white p-2 rounded-md w-full ${task.isCompleted && 'line-through'}`}
+          >
+            {task.title}
+          </button>
+          <button className="bg-slate-400 p-2 rounded-md text-white">
+            <ChevronRightIcon />
+          </button>
+          <button onClick={() => onDeleteTaskClick(task.id)} className="bg-slate-400 p-2 rounded-md text-white">
+            <TrashIcon />
+          </button>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default Tasks;
